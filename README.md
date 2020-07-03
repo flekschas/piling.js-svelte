77963b131a8620fa48c24bfd9254f5ca0e3d91c8
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-07-02 22:09:02 -0400

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Piling.js Svelte Demo with Rollup
+
+```
+git clone https://github.com/flekschas/piling.js-svelte
+cd piling.js-svelte
+npm install
+npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import createPiling from './piling-example.js'
+
+  let wrapper;
+
+  onMount(() => {
+    createPiling(wrapper);
+  });
+</script>
+
+<div bind:this={wrapper} id="pilingjs-wrapper"></div>
+
+<style>
+#pilingjs-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
